73fcfc4b8d47375de0f642e0d44f56402a134c77
Robert Scheller <rschell@ncsu.edu>
2019-03-10 09:53:45 -0400

    Initial commit.  This repository is experimental.

---
+++ Pixel.cs
@@ -0,0 +1,18 @@
+namespace Landis.Library.InitialCommunities
+{
+    public class Pixel : Landis.SpatialModeling.Pixel
+        //: SingleBandPixel<ushort>
+    {
+        public Pixel()
+            : base()
+        {
+        }
+
+        //---------------------------------------------------------------------
+
+        public Pixel(ushort band0)
+        //    : base(band0)
+        {
+        }
+    }
+}
